1a4f8f9420fa2cb1ccf9b96db5ab55851b0c8d53
mmathuria_707121 <muskanmathuria09@gmail.com>
2026-09-26 22:39:32 +0530

    Create Greatest.c

---
+++ Greatest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two number");
+    scanf ("%d%d",&a,&b);
+
+    if(a>b){
+        printf("%d is greatest",a);
+    }
+    else
+    {
+        printf("%d is greatest",b);
+    }
+}
