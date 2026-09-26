da3e76fa487be9b4d3d83dce476e623f43bfa675
mmathuria_707121 <muskanmathuria09@gmail.com>
2026-09-26 22:37:06 +0530

    Create last divisible by 7.c

---
+++ last divisible by 7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a, num;
+    printf("enter a number");
+    scanf("%d",num);
+    num=a%10;
+    if(num%7==0)
+    {
+        printf("Last digit is divisible by 7");
+    
+    }
+return 0;
+
+}
